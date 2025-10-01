801df5368151aa0d60a0e39f6d383bf84901c731
Jerome Gamo <jeromegamo@outlook.com>
2025-09-30 20:10:21 -0600

    chore: use return instead of break after transition

--- Runtime/StateMachine.cs
+++ Runtime/StateMachine.cs
@@ -60,7 +60,7 @@ namespace Ezhtellar.AI
             {
                 if (!t.Condition()) { continue; }
                 Transition(ActiveChild, t.Target);
-                break;
+                return;
             }
             
             ActiveChild?.Update();
